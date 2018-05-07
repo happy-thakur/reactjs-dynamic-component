266a9f3fe2dc2ba12fff13611641201aa40f9a8b
happy-thakur <suryanshsinghstudy@gmail.com>
2018-05-07 12:36:30 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import AppComponent from './appComp';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <AppComponent />
+      </div>
+    );
+  }
+}
+
+export default App;
